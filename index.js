ba55e6374a0632d0a5f4f8143c74c59b41013f92
samuelconlon <conlonsp@yahoo.com>
2025-01-07 13:37:07 -1000

    commit tree 4319f4d77a12fb5c19f71ee1219ac5f138cd1214

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'YOUR-NAME is the champion'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById('main').remove();
+
+// let newHeader = document.createElement('h1');
+
+// newHeader.id = 'victory';
+
+// newHeader.textContent = 'YOUR-NAME is the champion';
